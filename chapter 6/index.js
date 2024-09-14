d60f63172402db2ff98110ed98432a577e53d763
Talha Ahmed <talhaayjaz@gmail.com>
2024-09-14 07:09:10 +0500

    q 1 & 2

---
+++ chapter 6/index.js
@@ -0,0 +1,28 @@
+// Q 1
+
+var number = 10
+
+number = ++number
+console.log(number)
+
+number = number++
+console.log(number)
+
+number = number--
+console.log(number)
+
+number = --number
+console.log(number)
+
+
+// Q 2
+
+var a = 2;
+var b = 1;
+var result = --a - --b + ++b + b--;
+
+console.log(a)
+console.log(b)
+console.log(result)
+
+
